26b13d78da057d539abe90c0acb8c8c50f82b0c4
kimgihun1234 <y2k16241@naver.com>
2024-04-04 18:01:06 +0900

    mody

---
+++ example.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int board[20][20] = { 0, };
+int main() {
+	int dice[6] = { 0, };
+	int r, c, x, y,n,command;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cin >> r >> c >> y >> x >> n;
+	for (int i = 0; i < r; i++) {
+		for (int j = 0; j < c; j++) {
+			cin >> board[i][j];
+		}
+	}
+	while (n--) {
+		cin >> command;
+	}
+	cout << "hello";
+}
